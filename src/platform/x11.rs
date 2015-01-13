928666b936bf7442b97a03423ce0396e00f0a6a6
meh <meh@schizofreni.co>
2015-01-13 19:54:40 +0100

    platform/x11: update and implement everything

--- src/platform/x11.rs
+++ src/platform/x11.rs
@@ -17,6 +17,8 @@
 
 extern crate toml;
 
+use std::sync::mpsc::Sender;
+
 use clipboard;
 
 pub fn start(channel: Sender<clipboard::Message>, specs: Option<toml::Value>) -> Sender<clipboard::Change> {
@@ -47,11 +49,14 @@ mod lib {
 
 	use std::thread::Thread;
 	use std::sync::Arc;
+	use std::sync::mpsc::{Sender, Receiver, channel};
 
-	use std::hash;
+	use std::hash::{self, Hash, SipHasher};
 
 	use std::io::timer::sleep;
 	use std::time::duration::Duration;
+
+	use std::collections::BTreeMap;
 
 	use clipboard;
 	use clipboard::Direction::Outgoing;
@@ -74,10 +79,7 @@ mod lib {
 				let mut manager = Manager::new(main, display, source);
 
 				loop {
-					if let Ok(change) = receiver.try_recv() {
-						manager.serve(&change);
-					}
-					else {
+					if let None = manager.serve(&receiver) {
 						if let Some(change) = manager.poll() {
 							manager.send(&change);
 						}
@@ -86,13 +88,15 @@ mod lib {
 						sleep(Duration::seconds(1));
 					}
 				}
-			}).detach();
+			});
 
 			sender
 		}
 
 		pub fn new(main: Sender<clipboard::Message>, display: x::Display, source: String) -> Manager {
 			let window = x::Window::open(&display, &display.root(), (0, 0), (1, 1), (0, 0), 0);
+
+			window.select(x::PropertyChangeMask);
 
 			Manager {
 				channel: main,
@@ -102,7 +106,122 @@ mod lib {
 			}
 		}
 
-		pub fn serve(&self, change: &clipboard::Change) {
+		pub fn serve(&self, receiver: &Receiver<clipboard::Change>) -> Option<clipboard::Change> {
+			if let Some(c) = self.flush(receiver) {
+				let mut change = c;
+
+				self.window.selection_owner(self.intern(self.source.as_slice()), x::CurrentTime);
+
+				loop {
+					let event = self.display.next_event();
+
+					if let Some(..) = event.details::<x::SelectionClear>() {
+						return self.flush(receiver);
+					}
+
+					if let Some(details) = event.details::<x::SelectionRequest>() {
+						if let Some(c) = self.flush(receiver) {
+							change = c;
+						}
+
+						let name    = self.name(details.target);
+						let content = change.1.clone().into_iter().collect::<BTreeMap<String, Vec<u8>>>();
+
+						match name.as_slice() {
+							"TARGETS" => {
+								let mut targets: Vec<x::Atom> =
+									content.keys().map(|mime| self.intern(mime.as_slice())).collect();
+
+								if content.contains_key("text/plain") {
+									targets.push(self.intern("UTF8_STRING"));
+								}
+
+								self.set(details, &targets);
+							},
+
+							"UTF8_STRING" | "STRING" => {
+								if let Some(value) = content.get("text/plain") {
+									self.set(details, value);
+								}
+								else {
+									self.error(details);
+								}
+							},
+
+							name if Regex::new(r"^(.*?)/(.*?)$").unwrap().is_match(name) => {
+								if let Some(value) = content.get(name) {
+									self.set(details, value);
+								}
+								else {
+									self.error(details);
+								}
+							},
+
+							_ => {
+								self.error(details);
+							}
+						}
+					}
+				}
+			}
+
+			None
+		}
+
+		fn flush(&self, receiver: &Receiver<clipboard::Change>) -> Option<clipboard::Change> {
+			if let Ok(c) = receiver.try_recv() {
+				let mut change = c;
+
+				loop {
+					if let Ok(c) = receiver.try_recv() {
+						change = c;
+					}
+					else {
+						return Some(change);
+					}
+				}
+			}
+			else {
+				return None;
+			}
+		}
+
+		fn set<T>(&self, details: &x::SelectionRequest, data: &Vec<T>) {
+			let window = self.display.window(details.requestor);
+			let kind   = match self.name(details.target).as_slice() {
+				"TARGETS" => self.intern("ATOM"),
+				name      => self.intern(name)
+			};
+
+			let mut event = self.display.event();
+			{
+				let notify = event.mut_details::<x::SelectionNotify>();
+
+				notify.requestor = details.requestor;
+				notify.selection = self.intern(self.source.as_slice());
+				notify.target    = details.target;
+				notify.property  = details.property;
+				notify.time      = details.time;
+			}
+
+			window.set_property(details.property, kind, data);
+			window.send(&event);
+		}
+
+		fn error(&self, details: &x::SelectionRequest) {
+			let     window = self.display.window(details.requestor);
+			let mut event  = self.display.event();
+			{
+				let notify = event.mut_details::<x::SelectionNotify>();
+
+				notify.requestor = details.requestor;
+				notify.selection = self.intern(self.source.as_slice());
+				notify.target    = details.target;
+				notify.property  = 0;
+				notify.time      = details.time;
+			}
+
+			window.send(&event);
 		}
 
 		pub fn poll(&mut self) -> Option<clipboard::Change> {
@@ -110,36 +229,30 @@ mod lib {
 			static mut timestamp: u64 = 0;
 
 			let mut current: u64 = 0;
-
-			self.listen(x::PropertyChangeMask);
-
-			let mut content: Vec<clipboard::Content> = Vec::new();
+			let mut content: BTreeMap<String, Vec<u8>> = BTreeMap::new();
 			
-			if let Some(property) = self.selection("UTF8_STRING") {
-				content.push(("text/plain".to_string(),
-					property.items::<u8>().unwrap().to_vec()));
+			if let Some(property) = self.get("UTF8_STRING") {
+				content.insert("text/plain".to_string(),
+					property.items::<u8>().unwrap().to_vec());
 			}
-			else if let Some(property) = self.selection("STRING") {
-				content.push(("text/plain".to_string(),
-					property.items::<u8>().unwrap().to_vec()));
+			else if let Some(property) = self.get("STRING") {
+				content.insert("text/plain".to_string(),
+					property.items::<u8>().unwrap().to_vec());
 			}
 
-			if let Some(property) = self.selection("TARGETS") {
+			if let Some(property) = self.get("TARGETS") {
 				for atom in property.items::<x::Atom>().unwrap().iter() {
 					let name = self.name(*atom);
 
 					if Regex::new(r"^(.*?)/(.*?)$").unwrap().is_match(name.as_slice()) {
-						if let Some(value) = self.selection(name.as_slice()) {
-							content.push((name.clone(), value.items::<u8>().unwrap().to_vec()));
+						if let Some(value) = self.get(name.as_slice()) {
+							content.insert(name.clone(),
+								value.items::<u8>().unwrap().to_vec());
 						}
 					}
 					else {
-						match name.as_slice() {
-							"TIMESTAMP" => {
-								current = self.selection("TIMESTAMP").unwrap().items::<u32>().unwrap()[0] as u64;
-							},
-
-							_ => {}
+						if let "TIMESTAMP" = name.as_slice() {
+							current = self.get("TIMESTAMP").unwrap().items::<u32>().unwrap()[0] as u64;
 						}
 					}
 				}
@@ -161,7 +274,10 @@ mod lib {
 				}
 				else {
 					timestamp = 0;
-					current   = hash::hash(&content[0]);
+
+					for (ref key, ref value) in content.iter() {
+						current = hash::hash::<_, SipHasher>(&(current, key, value));
+					}
 
 					if current == hash {
 						return None;
@@ -171,11 +287,7 @@ mod lib {
 				}
 			}
 
-			Some(Arc::new((unsafe { timestamp }, content)))
-		}
-
-		fn listen(&self, mask: i64) {
-			self.window.select(mask);
+			Some(Arc::new((unsafe { timestamp }, content.into_iter().collect())))
 		}
 
 		fn intern(&self, name: &str) -> x::Atom {
@@ -186,7 +298,7 @@ mod lib {
 			self.display.atom_name(atom)
 		}
 
-		fn selection(&self, name: &str) -> Option<x::Property> {
+		fn get(&self, name: &str) -> Option<x::Property> {
 			let id = self.intern("SYMBIOTIC");
 
 			self.window.convert_selection(
@@ -212,7 +324,7 @@ mod lib {
 		}
 
 		pub fn send(&self, change: &clipboard::Change) {
-			self.channel.send(Outgoing(change.clone()));
+			self.channel.send(Outgoing(change.clone())).unwrap();
 		}
 	}
 
@@ -221,18 +333,24 @@ mod lib {
 		extern crate libc;
 
 		use self::libc::{c_void, c_int, c_uint, c_ulong, c_long, c_uchar};
+
 		use std::ops::Drop;
+
 		use std::ptr;
 		use std::mem;
-		use std::c_str::CString;
-		use std::slice;
-		use std::collections::BTreeMap;
 		use std::intrinsics::type_id;
+		use std::slice;
 
-		pub type Id   = c_ulong;
-		pub type Atom = c_ulong;
-		pub type Time = c_ulong;
-		pub type Bool = c_int;
+		use std::ffi::c_str_to_bytes;
+		use std::ffi::CString;
+
+		use std::collections::BTreeMap;
+
+		pub type Id     = c_ulong;
+		pub type Atom   = c_ulong;
+		pub type Time   = c_ulong;
+		pub type Bool   = c_int;
+		pub type Status = c_int;
 
 		pub static PropertyChangeMask: c_long = 1 << 22;
 
@@ -241,21 +359,42 @@ mod lib {
 
 		pub static CurrentTime: Time = 0;
 
-		#[deriving(Show)]
+		pub static PropModeReplace: c_int = 0;
+
+		#[derive(Show)]
 		#[repr(C)]
 		pub struct Event {
 			pub kind:    c_int,
 			pub serial:  c_ulong,
 			pub fake:    c_int,
 			pub display: *const c_void,
-			pub window:  Id,
 
-			_data: [u64, ..20u]
+			_data: [u64; 23us]
 		}
 
-		#[deriving(Show)]
+		#[derive(Show)]
 		#[repr(C)]
 		pub struct SelectionNotify {
+			pub requestor: Id,
+			pub selection: Atom,
+			pub target:    Atom,
+			pub property:  Atom,
+			pub time:      Time,
+		}
+
+		#[derive(Show)]
+		#[repr(C)]
+		pub struct SelectionClear {
+			pub window:    Id,
+			pub selection: Atom,
+			pub time:      Time,
+		}
+
+		#[derive(Show)]
+		#[repr(C)]
+		pub struct SelectionRequest {
+			pub owner:     Id,
+			pub requestor: Id,
 			pub selection: Atom,
 			pub target:    Atom,
 			pub property:  Atom,
@@ -263,14 +402,33 @@ mod lib {
 		}
 
 		impl Event {
-			pub fn details<T>(&self) -> Option<&T> where T: 'static {
+			pub fn new() -> Event {
 				unsafe {
-					if self.kind == 31 && type_id::<T>() == type_id::<SelectionNotify>() {
-						Some(::std::mem::transmute(&self._data))
+					mem::zeroed()
+				}
+			}
+
+			pub fn details<T>(&self) -> Option<&mut T> where T: 'static {
+				unsafe {
+					if (self.kind == 31 && type_id::<T>() == type_id::<SelectionNotify>()) ||
+					   (self.kind == 30 && type_id::<T>() == type_id::<SelectionRequest>()) ||
+					   (self.kind == 29 && type_id::<T>() == type_id::<SelectionClear>())
+					{
+						Some(mem::transmute(&self._data))
 					}
 					else {
 						None
 					}
+				}
+			}
+
+			pub fn mut_details<T>(&mut self) -> &mut T where T: 'static {
+				unsafe {
+					if type_id::<T>() == type_id::<SelectionNotify>() {
+						self.kind = 31;
+					}
+
+					mem::transmute(&self._data)
 				}
 			}
 		}
@@ -284,8 +442,8 @@ mod lib {
 			fn XDefaultRootWindow(display: *const c_void) -> Id;
 
 			fn XCreateSimpleWindow(display: *const c_void, parent: Id,
-														 x: c_int, y: c_int, width: c_uint, height: c_uint,
-														 border_width: c_uint, border: Id, background: Id) -> Id;
+			                       x: c_int, y: c_int, width: c_uint, height: c_uint,
+			                       border_width: c_uint, border: Id, background: Id) -> Id;
 
 			fn XSelectInput(display: *const c_void, window: Id, mask: c_long) -> c_int;
 
@@ -295,19 +453,28 @@ mod lib {
 
 			fn XNextEvent(display: *const c_void, event: *mut Event) -> c_int;
 
+			fn XSendEvent(display: *const c_void, window: Id, propagate: Bool,
+			              mask: c_long, event: *const Event) -> Status;
+
 			fn XConvertSelection(display: *const c_void, selection: Atom, target: Atom, property: Atom,
-													 requestor: Id, time: Time) -> c_int;
+			                     requestor: Id, time: Time) -> c_int;
 
 			fn XGetWindowProperty(display: *const c_void, w: Id, property: Atom,
-														long_offset: c_long, long_length: c_long,
-														delete: Bool, req_type: Atom,
-														actual_type_return: *mut Atom, actual_format_return: *mut c_int,
-														nitems_return: *mut c_ulong, bytes_after_return: *mut c_ulong,
-														prop_return: *mut *mut c_uchar) -> c_int;
+			                      long_offset: c_long, long_length: c_long,
+			                      delete: Bool, req_type: Atom,
+			                      actual_type_return: *mut Atom, actual_format_return: *mut c_int,
+			                      nitems_return: *mut c_ulong, bytes_after_return: *mut c_ulong,
+			                      prop_return: *mut *mut c_uchar) -> c_int;
+
+			fn XChangeProperty(display: *const c_void, window: Id, property: Atom,
+			                   kind: Atom, format: c_int, mode: c_int,
+			                   data: *const c_void, length: c_int) -> c_int;
 
 			fn XDeleteProperty(display: *const c_void, window: Id, property: Atom) -> c_int;
 
 			fn XMaxRequestSize(display: *const c_void) -> c_long;
+
+			fn XSetSelectionOwner(display: *const c_void, selection: Atom, owner: Id, time: Time) -> c_int;
 
 			fn XFree(data: *mut c_void) -> c_int;
 		}
@@ -331,7 +498,7 @@ mod lib {
 			id:      Id,
 		}
 
-		#[deriving(Show)]
+		#[derive(Show)]
 		pub struct Property<'a> {
 			pub id:   Atom,
 			pub kind: Atom,
@@ -345,13 +512,13 @@ mod lib {
 			pub fn items<T>(&self) -> Option<&'a [T]> {
 				if self.format == (mem::size_of::<T>() * 8) as i32 {
 					return unsafe {
-						Some(slice::from_raw_buf(mem::transmute(&self.data.as_ptr()), self.items as uint))
+						Some(slice::from_raw_buf(mem::transmute(&self.data.as_ptr()), self.items as usize))
 					}
 				}
 
 				if self.format == 32 && mem::size_of::<T>() == 8 {
 					return unsafe {
-						Some(slice::from_raw_buf(mem::transmute(&self.data.as_ptr()), (self.items / 2) as uint))
+						Some(slice::from_raw_buf(mem::transmute(&self.data.as_ptr()), (self.items / 2) as usize))
 					}
 				}
 
@@ -362,7 +529,7 @@ mod lib {
 		impl Display {
 			pub fn open(name: Option<&String>) -> Option<Display> {
 				let pointer = if let Some(name) = name {
-					unsafe { XOpenDisplay(name.to_c_str().as_ptr()) }
+					unsafe { XOpenDisplay(CString::from_slice(name.as_bytes()).as_ptr()) }
 				}
 				else {
 					unsafe { XOpenDisplay(ptr::null()) }
@@ -383,6 +550,13 @@ mod lib {
 				}
 			}
 
+			pub fn window(&self, id: Id) -> Window {
+				Window {
+					display: self.pointer,
+					id:      id,
+				}
+			}
+
 			pub fn next_event(&self) -> Event {
 				unsafe {
 					let mut event: Event = mem::uninitialized();
@@ -391,6 +565,14 @@ mod lib {
 
 					event
 				}
+			}
+
+			pub fn event(&self) -> Event {
+				let mut event = Event::new();
+
+				event.display = self.pointer;
+
+				return event;
 			}
 
 			pub fn intern_atom(&self, name: &str) -> Atom {
@@ -402,6 +584,10 @@ mod lib {
 
 						"SECONDARY" => {
 							2
+						},
+
+						"ATOM" => {
+							4
 						},
 
 						"STRING" => {
@@ -425,7 +611,7 @@ mod lib {
 						},
 
 						_ => {
-							XInternAtom(self.pointer, name.to_c_str().as_ptr(), False)
+							XInternAtom(self.pointer, CString::from_slice(name.as_bytes()).as_ptr(), False)
 						}
 					}
 				}
@@ -438,7 +624,7 @@ mod lib {
 
 				unsafe {
 					let buffer = XGetAtomName(self.pointer, id);
-					let result = String::from_str(CString::new(buffer, false).as_str().unwrap());
+					let result = String::from_utf8_lossy(c_str_to_bytes(&buffer)).into_owned();
 
 					XFree(buffer as *mut c_void);
 
@@ -446,6 +632,8 @@ mod lib {
 				}
 			}
 		}
+
+		unsafe impl Send for Display { }
 
 		impl Drop for Display {
 			fn drop(&mut self) {
@@ -457,8 +645,8 @@ mod lib {
 
 		impl Window {
 			pub fn open(display: &Display, parent: &Window,
-									(x, y): (i32, i32), (width, height): (u32, u32),
-									(border_width, border_id): (u32, Id), background: Id) -> Window {
+			            (x, y): (i32, i32), (width, height): (u32, u32),
+			            (border_width, border_id): (u32, Id), background: Id) -> Window {
 				Window {
 					display: display.pointer,
 
@@ -473,6 +661,12 @@ mod lib {
 			pub fn convert_selection(&self, selection: Atom, target: Atom, property: Atom) -> i32 {
 				unsafe {
 					XConvertSelection(self.display, selection, target, property, self.id, CurrentTime)
+				}
+			}
+
+			pub fn send(&self, event: &Event) -> Status {
+				unsafe {
+					XSendEvent(self.display, self.id, 0, 0, event)
 				}
 			}
 
@@ -491,7 +685,7 @@ mod lib {
 						return None;
 					}
 
-					data = Vec::from_raw_buf(buffer as *const u8, (items * ((format / 8) as u64)) as uint);
+					data = Vec::from_raw_buf(buffer as *const u8, (items * ((format / 8) as u64)) as usize);
 
 					XFree(buffer as *mut c_void);
 
@@ -504,6 +698,23 @@ mod lib {
 					})
 				}
 			}
+
+			pub fn set_property<T>(&self, id: Atom, kind: Atom, data: &Vec<T>) {
+				let (format, length) = match mem::size_of::<T>() * 8 {
+					8  => (8,  data.len()),
+					16 => (16, data.len()),
+					32 => (32, data.len()),
+					64 => (32, data.len() * 2),
+
+					s => panic!("unsupported size: {:?}", s)
+				};
+
+				unsafe {
+					XChangeProperty(self.display, self.id, id, kind, format, PropModeReplace,
+					                data.as_ptr() as *const c_void,
+					                length as c_int);
+				}
+			}
 			
 			pub fn delete_property(&self, id: Atom) {
 				unsafe {
@@ -514,6 +725,12 @@ mod lib {
 			pub fn select(&self, mask: c_long) {
 				unsafe {
 					XSelectInput(self.display, self.id, mask);
+				}
+			}
+
+			pub fn selection_owner(&self, selection: Atom, time: Time) {
+				unsafe {
+					XSetSelectionOwner(self.display, selection, self.id, time);
 				}
 			}
 		}
