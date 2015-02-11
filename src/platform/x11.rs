3966256244413d3cc9b293227f54fd19a60a46b6
meh <meh@schizofreni.co>
2015-02-12 00:20:15 +0100

    platform/x11: fix deprecation

--- src/platform/x11.rs
+++ src/platform/x11.rs
@@ -548,13 +548,13 @@ mod lib {
 			pub fn items<T>(&self) -> Option<&'a [T]> {
 				if self.format == (mem::size_of::<T>() * 8) as i32 {
 					return unsafe {
-						Some(slice::from_raw_buf(mem::transmute(&self.data.as_ptr()), self.items as usize))
+						Some(slice::from_raw_parts(mem::transmute(&self.data.as_ptr()), self.items as usize))
 					}
 				}
 
 				if self.format == 32 && mem::size_of::<T>() == 8 {
 					return unsafe {
-						Some(slice::from_raw_buf(mem::transmute(&self.data.as_ptr()), (self.items / 2) as usize))
+						Some(slice::from_raw_parts(mem::transmute(&self.data.as_ptr()), (self.items / 2) as usize))
 					}
 				}
 
